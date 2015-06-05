36bf3ef76219962170315fd752ec24bbe4c2c9b6
Koen van Zuijlen <khavanzuijlen@gmail.com>
2015-06-06 00:03:49 +0200

    Added Path string to view

--- src/js/controller/patheditor.js
+++ src/js/controller/patheditor.js
@@ -2,12 +2,18 @@ angular.module('pathEditor', [])
     .controller('pathEditorController', function($scope, $http) {
         $scope.paths = [];
 
+        //Retrieve PATH variable from system
         $scope.getPaths = function() {
             $http.post('path_io.php',
                 {action:'getpaths'})
                 .success(function(data) {
                     $scope.paths = data;
             });
+        };
+
+        //Get string to input back into PATH variable
+        $scope.getPathString = function() {
+            return $scope.paths.join(';');
         };
 
         $scope.getPaths();
